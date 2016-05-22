d7bbb3cbd74bf1e8dde7fa3165d37bb8b491c9d9
Felipe Calderon <felipe.calderon@arandasoft.com>
2016-05-22 14:03:53 -0500

    Implementation of ui tests

--- VOIQPokedexUITests/VOIQPokedexUITests.m
+++ VOIQPokedexUITests/VOIQPokedexUITests.m
@@ -17,8 +17,6 @@
 - (void)setUp {
     [super setUp];
     
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -28,13 +26,37 @@
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testDownloadAndShowPokemonList {
+    [XCUIDevice sharedDevice].orientation = UIDeviceOrientationPortrait;
+    
+    sleep(10);
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tables.staticTexts[@"Abomasnow"] tap];
+    
+    sleep(10);
+    [app.navigationBars[@"Abomasnow"].buttons[@"Pokemon List"] tap];
+    [app.tables.staticTexts[@"Abra"] tap];
+    
+    sleep(10);
+    [app.navigationBars[@"Abra"].buttons[@"Pokemon List"] tap];
+}
+
+- (void)testDownloadAndShowPokemonListLandscape {
+    [XCUIDevice sharedDevice].orientation = UIDeviceOrientationLandscapeRight;
+    
+    sleep(10);
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tables.staticTexts[@"Absol"] tap];
+    
+    sleep(10);
+    [app.navigationBars[@"Absol"].buttons[@"Pokemon List"] tap];
+    [app.tables.staticTexts[@"Accelgor"] tap];
+    
+    sleep(10);
+    [app.navigationBars[@"Accelgor"].buttons[@"Pokemon List"] tap];
 }
 
 @end
